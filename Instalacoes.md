4e0e0fe0ebc39175a88ecb7248897cc2cd8f9880
Matheus Henrique <matheushlm2@gmail.com>
2022-11-12 11:15:05 -0300

    #

--- Instalacoes.md
+++ Instalacoes.md
@@ -1,3 +1,4 @@
 Package.json = npm init -y
 Express  = npm i -save express
-Nodemon = npm i --save-dev nodemon(Voltado pra desenvolvimento)
+Nodemon = npm i --save-dev nodemon (Voltado pra desenvolvimento)
+PM2 = npm i --save pm2 (Producao)
--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const express = require('express')
 const app = express()
 
+app.use("/opa",(req, res, next) => {
+    console.log('Antes')
+    next()
+})
+
+app.get("/opa",(req, res, next) => {
+    // res.send("<h1>Estou bem e funcional</h1>")
+    /*res.json({
+        name: 'God of War Ragnarok',
+        price: 299,
+        Genre: 'Aventure'
+    })*/
+    console.log('Durante')
+    res.json([
+        {id:1,
+         nome: "Matheus"},
+        {id: 2,
+         nome: "Giovanny"
+        },
+        {id: 3,
+         nome: "Isla"
+        }
+    ])
+    next()
+})
+
+    app.use("/opa", (req, res, next) => {
+        console.log('Depois')
+    })
+
 app.listen(3000, () => {
     console.log('Servidor Iniciado')
 })
+

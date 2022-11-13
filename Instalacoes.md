052da3ba62e64b1d4aa0bf9ecbb2888423fcff11
Matheus Henrique <matheushlm2@gmail.com>
2022-11-13 11:04:31 -0300

    #

--- Instalacoes.md
+++ Instalacoes.md
@@ -2,3 +2,4 @@ Package.json = npm init -y
 Express  = npm i -save express
 Nodemon = npm i --save-dev nodemon (Voltado pra desenvolvimento)
 PM2 = npm i --save pm2 (Producao)
+npm i --save body-parser(Caso queira usar body parser)
--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require('express')
+const saudacao = require('./saudacaoMid')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.text())
+app.use(bodyParser.json)
+app.use(saudacao('Davi'))
 
 app.use("/opa",(req, res, next) => {
     console.log('Antes')
     next()
 })
+
+app.get("/clientes/relatorio", (req, res, next) => {
+    res.send(`Cliente relatório: completo ${req.query.completo} ano = ${req.query.ano}`)
+    next()
+})
+ 
+app.post('corpo', (req, res, next) => {
+    /*let corpo = ''
+    req.on('data', function(parte) {
+        corpo += parte
+    })
+    req.on('end', function() {
+        req.send(corpo)
+    })*/
+
+    req.send(req.body.nome) //precisa ter feito o app.use(bodyParser.text()) lá em cima pode ser outro tmb como o .json
+    next()
+})
+
+app.get("/clientes/:id", (req, res) => {
+    res.send(`Cliente ${req.params.id} selecionando!`)
+})
+
+
 
 app.get("/opa",(req, res, next) => {
     // res.send("<h1>Estou bem e funcional</h1>")
